91c18fcbe564a5b150b9d12e5b0a2809b873c70f
Crowdbotics <engineering@crowdbotics.com>
2023-08-31 23:48:09 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "autumn-wildflower--113287",
+  url: "https://autumn-wildflower--113287.botics.co",
+  api: "https://autumn-wildflower--113287.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
